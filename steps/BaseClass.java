c367d2a2f0dd1234df415577acba080bffcb729f
ShrutiGoswami17 <93458813+ShrutiGoswami17@users.noreply.github.com>
2021-12-17 19:16:03 +0530

    Add files via upload

---
+++ steps/BaseClass.java
@@ -0,0 +1,17 @@
+package steps;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass extends AbstractTestNGCucumberTests{
+	
+	public static ChromeDriver driver;
+	
+	
+	
+
+}
---
+++ steps/HookClass.java
@@ -0,0 +1,31 @@
+package steps;
+
+import java.time.Duration;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HookClass  extends BaseClass {
+
+
+	@Before
+	public void preCondition() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+	}
+
+	@After
+	public void postCondition() {
+
+	driver.close();
+	}
+
+	}
